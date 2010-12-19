9e5ebdc55be6fe07bd8591ac5bcc4fe9c8a6c244
Feross Aboukhadijeh <feross@gmail.com>
2010-12-19 12:11:08 -0800

    Auto-advance to next song, use Last.fm API to show currently playing song info, album art.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 // TODO:
 // Add fb:comments after video is loaded so we don't block
-// Auto-advance to next song
+// Auto-advance to next song **
 // Keyboard shortcuts
+// Show play button on table row hover
 
 // FB login to save all your playlists
 // FB login to see your friends' playlists
 // FB login to see recommended playlists based on Likes
 
-// Show currently playing song info, album art, links to buy
+// Show currently playing song info, album art **
+// Show empty album art when unavailable
+// Show links to buy
 
 // Bug: Firefox restarts video on scroll
 
@@ -15,7 +18,7 @@ var controller;
 var ytplayer;
 
 $(function() {
-    controller = new Controller([{t:"Buddy Holly", a:"Weezer"}, {t:"Walid Toufic", a:"La T'awedny Aleik"}, {t:"Stylo", a:"Gorillaz"}, {t:"Smells Like Teen Spirit", a:"Nirvana"}, {t:"Eenie Meenie", a:"Justin Bieber"}, {t:"Replay", a:"Iyaz"}, {t:"Sweet Talking Woman", a:"ELO"}, {t:"Wavin Flag", a:"Knaan"}, {t:"Still Alive", a:"Glados"}, {t:"Stylo 2", a:"Gorillaz"}, {t:"Smells Like Teen Spirit", a:"Nirvana"}, {t:"Eenie Meenie", a:"Justin Bieber"}, {t:"Replay", a:"Iyaz"}, {t:"Sweet Talking Woman", a:"ELO"}, {t:"Wavin Flag", a:"Knaan"}, {t:"Still Alive", a:"Glados"}, {t:"Stylo 3", a:"Gorillaz"}, {t:"Smells Like Teen Spirit", a:"Nirvana"}, {t:"Eenie Meenie", a:"Justin Bieber"}, {t:"Replay", a:"Iyaz"}, {t:"Sweet Talking Woman", a:"ELO"}, {t:"Wavin Flag", a:"Knaan"}, {t:"Still Alive", a:"Glados"}, {t:"Stylo 4", a:"Gorillaz"}, {t:"Smells Like Teen Spirit", a:"Nirvana"}, {t:"Eenie Meenie", a:"Justin Bieber"}, {t:"Replay", a:"Iyaz"}, {t:"Sweet Talking Woman", a:"ELO"}, {t:"Wavin Flag", a:"Knaan"}, {t:"Still Alive", a:"Glados"}]);
+    controller = new Controller({title: 'Feross\'s Party Mix', songs: [{t:"Replay", a:"Iyaz"}, {t:"Buddy Holly", a:"Weezer"}, {t:"Walid Toufic", a:"La T'awedny Aleik"}, {t:"Stylo", a:"Gorillaz"}, {t:"Smells Like Teen Spirit", a:"Nirvana"}, {t:"Eenie Meenie", a:"Justin Bieber"}, {t:"Sweet Talking Woman", a:"ELO"}, {t:"Wavin Flag", a:"Knaan"}, {t:"Still Alive", a:"Glados"}]});
     controller.playlist.render();
     controller.playlist.playSong(0); // Auto-play
     
@@ -39,12 +42,23 @@ function onYouTubePlayerReady(playerId) {
     ytplayer.addEventListener("onStateChange", "onPlayerStateChange");
 }
 
+function onPlayerStateChange(newState) {
+    if (newState == 0) { // finished a video
+        controller.playlist.playNextSong();
+    }
+}
+
 /**
  * Instant.fm Controller
  */
 var Controller = function(playlist) {
     this.isPlayerInitialized = false; // have we called initPlayer?
     this.playlist = new Playlist(playlist);
+    
+	this.lastfm = new LastFM({
+		apiKey    : '414cf82dc17438b8c880f237a13e5c09',
+		apiSecret : '02cf123c38342b2d0b9d3472b65baf82',
+	});
 }
 /**
  * Controller.initPlayer() - Initialize the YouTube player
@@ -104,8 +118,12 @@ Controller.prototype.playVideoById = function(id) {
 /**
  * Instant.fm Playlist
  */
-var Playlist = function(songs) {
-    this.songs = songs || [];
+var Playlist = function(playlist) {
+    if (!playlist) {
+        return;
+    }
+    this.title = playlist.title;
+    this.songs = playlist.songs || [];
 }
 
 /**
@@ -113,22 +131,46 @@ var Playlist = function(songs) {
  * @i - song index
  */
 Playlist.prototype.playSong = function(i) {
+    if (i >= this.songs.length) {
+        return;
+    }
+    this.curSongIndex = i;
     var s = this.songs[i];
     var q = s.t + ' ' + s.a;
     controller.playVideoBySearch(q);
  
     $('.playing').removeClass('playing');
     $('#song' + i).children().first().addClass('playing');
+    
+    $('#curSong').text(s.t);
+    $('#curArtist').text(s.a);
+	controller.lastfm.track.getInfo({track: s.t, artist: s.a}, {success: function(data){
+		var t = data.track;
+		if (t && t.album && t.album.image) {
+		    imgSrc = t.album.image[t.album.image.length - 1]['#text'];
+		    $('#curAlbumArt')
+		        .replaceWith($('<img alt="'+t.album.title+'" id="curAlbumArt" src="'+imgSrc+'" />'));
+		}
+		log(t);
+	}, error: function(code, message){
+		log(code);
+		log(message);
+	}});
+}
+
+Playlist.prototype.playNextSong = function() {
+    this.playSong(++this.curSongIndex);
 }
 
 /**
  * Playlist.render() - Updates the playlist table
  */
 Playlist.prototype.render = function() {
-    $('.song').remove();
+    $('#curPlaylist').text(this.title);
     
+    $('.song').remove();
     $.each(this.songs, function(i, v) {
-        $('<tr class="song pointer" id="song'+i+'"><td class="icon">&nbsp;</td> <td class="title">'+v.t+'</td><td class="artist">'+v.a+'</td></tr>').appendTo($('#playlist'));
+        $('<tr class="song pointer" id="song'+i+'"><td class="icon">&nbsp;</td> <td class="title">'+v.t+'</td><td class="artist">'+v.a+'</td></tr>').appendTo('#playlist');
     });
     
     $('.song:odd').addClass('odd');
