02819381f3c30cd6f0346730a2e1c903035eabd6
Feross Aboukhadijeh <feross@gmail.com>
2010-12-26 02:40:46 -0800

    New logo. Fixed bug where scrollbar was too tall on short playlists.

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,3 +1,73 @@
+var model;
+var view;
+var controller;
+
+var settings = {
+    fancyZoom : { directory: '/images/fancyzoom' },
+};
+
+// Onload event
+$(function() {
+    view = new View();
+    model = new Model();
+    controller = new Controller();
+    
+    controller.loadPlaylist(initial_playlist);
+    
+    setupPlaylistDisplay();
+    setupKeyboardListeners();
+
+    $('#helpLink').fancyZoom(settings.fancyZoom);
+    
+    window.onpopstate = controller.onPopState;
+    $(window).resize(setupPlaylistDisplay);
+    
+    new uploader('container', null, '/upload', null, controller.loadPlaylist); // HTML5 dragdrop upload
+});
+
+function setupPlaylistDisplay() {
+    var maxPlaylistHeight = $(window).height() - $('#videoDiv').height() - $('#header').height();
+    var newHeight = Math.min($('#playlist').height(), maxPlaylistHeight);
+    $('#playlistDiv').height(newHeight);
+}
+
+// Set up keyboard shortcuts in a cross-browser manner. (Tested in Firefox, Chrome, Safari)
+// Keyboard events are a mess: http://www.quirksmode.org/js/keys.html
+// TODO: Test in IE
+function setupKeyboardListeners() {
+    
+    // Detect keys
+    $(window).keydown(function(event) {
+        var k = event.which;
+        
+        if (k == 39 || k == 40) { // down, right
+            controller.playNextSong();
+        } else if (k == 37 || k == 38) { // up, left
+            controller.playPrevSong();
+        } else if (k == 32) { // space
+            view.playPause();
+        } else {
+            return true; // default event
+        }
+        
+        event.preventDefault(); // prevent default event    
+    });
+    
+    // Detect characters
+    $(window).keypress(function(event) {
+        var k = event.charCode || event.keyCode;
+              
+        if (k == 63) { // ? mark character
+            $('#helpLink').click();
+        } else {
+            return true; // default event
+        }
+        
+        event.preventDefault(); // prevent default event    
+    });
+}
+
+
 var Controller = function() {
     this.songIndex; // Current position in the playlist
     this.queuedSong;
