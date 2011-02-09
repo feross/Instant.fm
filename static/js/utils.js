8cf801cbd552f9cdf30bcbf9113589cc7bac9d01
Jake Becker <jabecker@stanford.edu>
2011-02-08 23:24:27 -0800

    Fixed bug with new playlist javascript handling. Re-added missing javascript log-in functions. In short, fixed a few merge bugs.

--- static/js/utils.js
+++ static/js/utils.js
@@ -111,3 +111,56 @@ window.log = function(){
     if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
   };
 })(document);
+
+
+/* ------------------------ LOGIN ------------------------------*/
+
+/* The following three functions are from QuirksMode.org */
+function createCookie(name,value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else var expires = "";
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  createCookie(name,"",-1);
+}
+
+/* Call this right after the user is logged in or out to update
+ * display. 
+ */
+function loginStatusChanged() {
+    var user_id = readCookie('user_id');
+    var user_name = readCookie('user_name');
+    if (user_id && user_name) {
+        $('html').addClass('loggedIn');
+        $('html').removeClass('loggedOut');
+        $('.username').html(unescape(user_name));
+    } else {
+        $('html').addClass('loggedOut');
+        $('html').removeClass('loggedIn');
+    }
+}
+
+function logout() {
+    eraseCookie('user_id');
+    eraseCookie('user_name');
+    eraseCookie('session_id');
+    loginStatusChanged();
+    return false;
+}
